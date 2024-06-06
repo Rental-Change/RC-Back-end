04194fee4060b37722aa4297323b6a5fa3fa5fe9
leesuyeon <lgh0385hh@gmail.com>
2024-06-06 19:50:37 +0900

    FIX: 토큰 재발급 수정

--- token/login-required.js
+++ token/login-required.js
@@ -1,21 +1,32 @@
-// login-required.js
+const jwt = require('jsonwebtoken');
+const secretKey = 'asdfghj1233334';
+
 function loginRequired(req, res, next) {
     try {
         // 요청 헤더에서 액세스 토큰 가져오기
         const accessToken = req.headers.authorization.split(" ")[1];
-        console.log(accessToken)
+        // AccessToken 디코딩하여 유효성 검사
+        jwt.verify(accessToken, secretKey, (err, decoded) => {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    // 만료된 토큰인 경우
+                    res.status(401).json({ success: false, message: '토큰이 만료되었습니다.' });
+                } else {
+                    // 그 외의 오류인 경우
+                    console.error('Error verifying token:', err);
+                    res.status(500).json({ success: false, message: '서버 오류 발생' });
+                }
+            } else {
+                res.status(200).json({ success: true, message: "토큰 유효"})
+                // 액세스 토큰이 유효한 경우
+                next(); // 다음 미들웨어로 넘어감
+            }
+        });
     
-         // 액세스 토큰 유효성 검사
-         if (!accessToken) {
-          return res.status(401).json({ success: false, message: '유효하지 않은 토큰입니다.' });
-         }
-    
-        // 액세스 토큰이 유효하면 보호된 데이터를 반환
-        res.json({ success: true, data: 'This is protected data' });
-    
-      } catch (error) {
+    } catch (error) {
         console.error('Protected endpoint error:', error);
-      }
+        res.status(500).json({ success: false, message: '서버 오류 발생' });
+    }
 }
 
 module.exports = loginRequired;
--- token/refreshJwtMiddleware.js
+++ token/refreshJwtMiddleware.js
@@ -1,17 +1,21 @@
 const jwt = require('jsonwebtoken'); // jwt 모듈 불러오기
+const secretKey = 'asdfghj1233334';
+const { generateAccessToken }= require('./jwt');
 
 const refreshJwtMiddleware = (req, res, next) => {
-  const token = req.cookies.refreshToken;
+  const refreshToken = req.cookies.refreshToken; // 리프레시 토큰 가져오기
+  
+  if (!refreshToken) return res.sendStatus(401); // 리프레시 토큰이 없으면 401 Unauthorized 반환
 
-  if (!token) return res.sendStatus(401);
-
-  jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-
-    const accessToken = generateAccessToken({ name: user.name });
+  jwt.verify(refreshToken, secretKey, (err, decoded) => {
+    if (err) return res.sendStatus(403); // 리프레시 토큰이 유효하지 않으면 403 Forbidden 반환
+    
+    // 리프레시 토큰이 유효하면 새로운 액세스 토큰 생성
+    const accessToken = generateAccessToken({ userId: decoded.userId });
+    
+    // 새로운 액세스 토큰을 클라이언트에게 전달
     res.json({ accessToken: accessToken });
   });
-
 }
 
 // 미들웨어 함수 내보내기
