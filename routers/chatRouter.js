c327a1e145d05ab4274f2159aab1884fe91141ef
leesuyeon <lgh0385hh@gmail.com>
2024-06-06 20:45:38 +0900

    FIX

--- routers/chatRouter.js
+++
@@ -1,9 +0,0 @@
-//chatRouter.js
-
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controller/chatController')
-
-router.get('/chat', chatController.createChat);
-
-module.exports = router;
--- routers/signupRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-router.post('/', userController.createUser);
-
-module.exports = router;
